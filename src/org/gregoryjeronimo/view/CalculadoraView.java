32750f004f21ced968d8dc8d1ea757a486215976
Gregory Jeronimo <gjeronimo-2026116@kinal.edu.gt>
2026-07-15 17:22:00 -0600

    003 se agrego View y MenuPrincipal

---
+++ src/org/gregoryjeronimo/view/CalculadoraView.java
@@ -0,0 +1,41 @@
+package org.gregoryjeronimo.view;
+
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.control.Label;
+import javafx.scene. layout. GridPane;
+import javafx.scene.layout. VBox;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+/**
+ *
+ * @author Gregory Jeronimo
+ */
+
+
+public class CalculadoraView {
+private VBox view;
+private Label pantalla;
+private final   GridPane cuadroBotones;
+
+//Controller 
+
+public CalculadoraView() {
+view = new VBox(15);
+view.setPadding(new Insets(15));
+view.setAlignment(Pos.CENTER);
+view.setStyle("-fx-background-color: #4A5D23;'");
+
+pantalla = new Label("0");
+pantalla.setFont(Font.font("Consolas",FontWeight.BOLD, 40));
+pantalla.setAlignment(Pos.CENTER_RIGHT);
+pantalla.setPrefSize(255, 50);
+cuadroBotones = new GridPane();
+
+view.getChildren().addAll(pantalla, cuadroBotones);
+        }
+public VBox  getView(){
+ return view;
+}
+
+}
