cc47eecd93348e55040d171b111bb75f34725db8
Gregory Jeronimo <informatica@alumno.local>
2026-07-17 09:17:30 -0600

    Se agregaron mas botones en view

--- src/org/gregoryjeronimo/view/CalculadoraView.java
+++ src/org/gregoryjeronimo/view/CalculadoraView.java
@@ -48,21 +48,29 @@ public class CalculadoraView {
         Button btnSiete = nuevoBoton("7");
         Button btnOcho = nuevoBoton("8");
         Button btnNueve = nuevoBoton("9");
-         Button btnCero = nuevoBoton("0");
+        Button btnCero = nuevoBoton("0");
+        Button btnMenos = nuevoBoton("-");
+          Button btnDivicion = nuevoBoton("/");
+            Button btnMultiplicacion = nuevoBoton("*");
+              
+        
         cuadroBotones.add(btnUno, 0, 3);
         cuadroBotones.add(btnDos, 1, 3);
         cuadroBotones.add(btnTres, 2, 3);
-        cuadroBotones.add(btnMas, 3, 3);
-        cuadroBotones.add(btnIgual, 3, 4);
-         cuadroBotones.add(btnClean, 0, 6);
-          cuadroBotones.add(btnCuatro, 1, 4);
-          cuadroBotones.add(btnCinco, 2, 4);
-          cuadroBotones.add(btnSeis, 2, 4);
-          cuadroBotones.add(btnSiete, 0, 5);
-          cuadroBotones.add(btnOcho, 1, 5);
-          cuadroBotones.add(btnNueve, 2, 5);
-          cuadroBotones.add(btnCero, 3, 5);
-          
+        cuadroBotones.add(btnMas, 3, 4);
+        cuadroBotones.add(btnIgual, 3, 6);
+        cuadroBotones.add(btnClean, 3, 3);//3,6
+        cuadroBotones.add(btnCuatro, 1, 4);
+        cuadroBotones.add(btnMenos, 3, 5);
+        cuadroBotones.add(btnCinco, 0, 4);
+        cuadroBotones.add(btnSeis, 2, 4);
+        cuadroBotones.add(btnSiete, 0, 5);
+        cuadroBotones.add(btnOcho, 1, 5);
+        cuadroBotones.add(btnNueve, 2, 5);
+        cuadroBotones.add(btnCero, 0, 6);
+         cuadroBotones.add(btnDivicion, 1, 6);
+         cuadroBotones.add(btnMultiplicacion, 2, 6);
+
 
         view.getChildren().addAll(pantalla, cuadroBotones);
     }
